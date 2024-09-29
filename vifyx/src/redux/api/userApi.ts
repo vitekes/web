0b6982ce1e41f716f9bb12566d2f260ed488af27
Kirill Vegele <kirillvegele10@gmail.com>
2024-09-29 12:45:17 +0300

    Add .env and small updates

--- vifyx/src/redux/api/userApi.ts
+++ vifyx/src/redux/api/userApi.ts
@@ -1,26 +1,27 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { API_URL } from '../../constants/api.constants'
 
 export const userApi = createApi({
-  reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://167.172.96.11/api/v1/users/",
-  }),
-  endpoints: (builder) => ({
-    registration: builder.mutation({
-      query: (body) => ({
-        url: "/registration/",
-        method: "POST",
-        body,
-      }),
-    }),
-    login: builder.mutation({
-      query: (body) => ({
-        url: "/login/",
-        method: "POST",
-        body,
-      }),
-    }),
-  }),
-});
+	reducerPath: 'userApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: `${API_URL}/v1/users/`,
+	}),
+	endpoints: builder => ({
+		registration: builder.mutation({
+			query: body => ({
+				url: '/registration/',
+				method: 'POST',
+				body,
+			}),
+		}),
+		login: builder.mutation({
+			query: body => ({
+				url: '/login/',
+				method: 'POST',
+				body,
+			}),
+		}),
+	}),
+})
 
-export const { useRegistrationMutation, useLoginMutation } = userApi;
+export const { useRegistrationMutation, useLoginMutation } = userApi
