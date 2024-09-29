8c4b43fa81efbdc19625e24348c9419e73f3f5fd
Kirill Vegele <kirillvegele10@gmail.com>
2024-09-29 23:26:27 +0300

    add Api & Services

---
+++ vifyx/src/services/comments.service.ts
@@ -0,0 +1,16 @@
+import { axiosClassic } from '../api/axios'
+
+class CommentsService {
+	private readonly BASE_URL = '/comments'
+
+	public async getAll() {
+		const { data } = await axiosClassic.get(this.BASE_URL)
+		return data
+	}
+	public async getOne(id: string) {
+		const { data, status } = await axiosClassic.get(this.BASE_URL + `/${id}`)
+		if (status === 404) throw new Error('Comment not found!')
+		return data
+	}
+}
+export default new CommentsService()
